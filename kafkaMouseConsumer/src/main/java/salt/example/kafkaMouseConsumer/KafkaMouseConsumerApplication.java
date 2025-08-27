322111231a33b29badd6962d4c8085a4ffcce114
David Aslan <davidaslan112671@live.se>
2025-08-27 12:56:44 +0200

    added kafka config and listener basic structure

---
+++ kafkaMouseConsumer/src/main/java/salt/example/kafkaMouseConsumer/KafkaMouseConsumerApplication.java
@@ -0,0 +1,13 @@
+package salt.example.kafkaMouseConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMouseConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMouseConsumerApplication.class, args);
+	}
+
+}
